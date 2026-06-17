2db332aad0551ed55d6acf6ee422e468349b508d
Sagor8187 <sdsagor8187@gmail.com>
2026-06-17 22:14:16 +0600

    job post route create successful

--- index.js
+++ index.js
@@ -21,16 +21,26 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+     await client.connect();
      const database = client.db("job-portal");
     const Jobcollection = database.collection("jobs");
+
+    //Recruiter Job post api 
+    app.post("/api/jobs",async(req,res)=>{
+      const job = req.body;
+      const result =await Jobcollection.insertOne(job)
+      res.status(201).send({success: true,message: "Job created successfully",result});
+    })
+
+
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+   
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
